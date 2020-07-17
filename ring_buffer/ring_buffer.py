8322afd33f7d079e50b63f506587f70dedfffc6b
Alexander Karren <alex@karren.com>
2020-07-17 09:33:45 -0600

    completed 'Implement a data structure called a ring buffer (more details below)'

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.oldest_index = 0
+        self.storage = []
 
     def append(self, item):
-        pass
+        if len(self.storage) >= self.capacity:
+            self.storage.pop(self.oldest_index)
+            self.storage.insert(self.oldest_index, item)
+            if self.oldest_index >= self.capacity - 1:
+                self.oldest_index = 0
+            else:
+                self.oldest_index += 1
+        else:
+            self.storage.append(item)
 
     def get(self):
-        pass
+        return [x for x in self.storage if x is not None]
